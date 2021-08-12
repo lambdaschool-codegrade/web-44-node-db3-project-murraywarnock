1ee94601bf2a675eff54ef5a3fea8638769de0ce
MurrayWarnock <murray.warnock@gmail.com>
2021-08-11 22:53:46 -0600

    scheme_model.js findSteps

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -120,7 +120,7 @@ async function findById(scheme_id) { // EXERCISE B
     return result;
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -141,16 +141,23 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
-  try {
-    return db('schemes as sc')
+  // try {
+    const rows = await db('schemes as sc')
       .leftJoin('steps as st', 'sc.scheme_id', '=', 'st.scheme_id')
-      .select('sc.scheme_name, st.*')
+      .select(
+        'st.step_id', 
+        'st.step_number',
+        'st.instructions',
+        'sc.scheme_name')
       .where('sc.scheme_id', '=', scheme_id)
-      .orderBy('sc.scheme_id ');
+      .orderBy('st.step_number ');
 
-  } catch (error) {
-    return (error);
-  }
+      if (!rows[0].step_id) return[];
+      return rows;
+
+  // } catch (error) {
+  //   return (error);
+  // }
 }
 
 function add(scheme) { // EXERCISE D
